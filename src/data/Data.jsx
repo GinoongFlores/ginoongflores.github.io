86c1549df3128df2dc4c921fb5b58391e83d0279
Christian Paul Flores <florespaul161@gmail.com>
2023-12-25 17:44:58 +0800

    add data

---
+++ src/data/Data.jsx
@@ -0,0 +1,61 @@
+const projectsData = [
+	{
+		title: "GDSC STI CDO Website",
+		description:
+			"A landing page of the Google Developer Student Clubs STI College CDO",
+		imgSrc: "dscsticdo-website.png",
+		imgAlt: "GDSC STI CDO website",
+		// if not available use # instead of link
+		visitLink: "https://dscsticdo.github.io/",
+		visitRepo: "",
+	},
+	{
+		title: "Panaghiusa Website",
+		description:
+			"A landing page website of the Panaghiusa application, Google Solution Challenge 2022 project.",
+		imgSrc: "panaghiusa-website.png",
+		imgAlt: "Panaghiusa website",
+		visitLink: "https://panaghiusa.netlify.app/",
+		visitRepo: "test",
+	},
+	{
+		title: "Password Generator",
+		description:
+			"A simple password generator that generates a random passphrase",
+		imgSrc: "passwordGenerator-website.png",
+		imgAlt: "Password Generator website",
+	},
+	{
+		title: "Scoreboard",
+		description:
+			'Scoreboard is a simple web application that counts "home" and "guest" scores.',
+		imgSrc: "scoreboard-website.png",
+		imgAlt: "Scoreboard website",
+	},
+];
+
+const skillsData = [
+	{
+		title: "Web Development",
+		description:
+			"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+		imgSrc: "web-development.png",
+		imgAlt: "Meaningful alt text for an image that is not purely decorative",
+	},
+	{
+		title: "SEO",
+		description:
+			"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+		imgSrc: "seo.png",
+		imgAlt: "Meaningful alt text for an image that is not purely decorative",
+	},
+	{
+		title: "Content & Technical Writing",
+		description:
+			"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+		imgSrc: "content-writing.png",
+		imgAlt: "Meaningful alt text for an image that is not purely decorative",
+	},
+];
+
+export { projectsData, skillsData };
