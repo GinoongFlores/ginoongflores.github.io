ed371db39d351477f6c7b6499b825b5a74c4711f
Christian Paul Flores <florespaul161@gmail.com>
2023-12-29 10:59:04 +0800

    ref: ESLint and add Prettier configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-    }
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
-
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {},
+		container: {
+			center: true,
+			padding: "1rem",
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
