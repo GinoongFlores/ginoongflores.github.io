9009ac09115c7297f96a4953d25575dc8889d461
ginoongflores <florespaul161@gmail.com>
2024-10-08 21:16:48 +0800

    feat(Skills): make use of components and get data from different file to get info skills.

---
+++ src/data/skills.jsx
@@ -0,0 +1,26 @@
+import { RiFileSearchFill } from "react-icons/ri";
+import { MdOutlineWeb, MdContentCopy } from "react-icons/md";
+
+export const skills = [
+	{
+		id: 1,
+		title: "Web Development",
+		description:
+			"I'm keen and eager to build coded static websites such as landing pages and portfolio websites for businesses and professionals.",
+		Icon: MdOutlineWeb,
+	},
+	{
+		id: 2,
+		title: "SEO",
+		description:
+			"I am constantly improving my SEO skill set using Google Analytics and Microsoft Clarity.",
+		Icon: RiFileSearchFill,
+	},
+	{
+		id: 3,
+		title: "Content & Technical Writing",
+		description:
+			"I aspired to write web content for my client's landing pages and am looking to contribute to technical writing using my skills as an IT student.",
+		Icon: MdContentCopy,
+	},
+];
